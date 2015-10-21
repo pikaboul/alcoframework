87d20f3bf8a3679c169a6b227f28ef8b82f7dafb
BOULONGNE Thomas <tboulongne@p-028-01.intra.gobelins.fr>
2015-10-21 15:03:01 +0200

    Mode d'emploi tahu

--- template/tpl.php
+++ template/tpl.php
@@ -8,24 +8,32 @@
 </head>
 <body>
 	<div class="content">
-		<?php if (count($users) > 0): ?>
-			<?php foreach ($users as $key => $value): ?>
-				<?php Engine::render('user', array("value"=>$value, "key"=>$key)); ?>
-			<?php endforeach ?>
-		<?php else: ?>
-			<div class="hint">Press 
-				<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-					 viewBox="0 0 26 26" style="enable-background:new 0 0 26 26;" xml:space="preserve">
-				<g>
-					<path style="stroke:#Turquoise;" d="M25,2H9C8.449,2,8,2.449,8,3c0,0,0,7,0,9s-2,2-2,2H1c-0.551,0-1,0.449-1,1v8c0,0.551,0.449,1,1,1h24
-						c0.551,0,1-0.449,1-1V3C26,2.449,25.551,2,25,2z M22,14c0,1.436-1.336,4-4,4h-3.586l1.793,1.793c0.391,0.391,0.391,1.023,0,1.414
-						C16.012,21.402,15.756,21.5,15.5,21.5s-0.512-0.098-0.707-0.293l-3.5-3.5c-0.391-0.391-0.391-1.023,0-1.414l3.5-3.5
-						c0.391-0.391,1.023-0.391,1.414,0s0.391,1.023,0,1.414L14.414,16H18c1.398,0,2-1.518,2-2v-2c0-0.553,0.447-1,1-1s1,0.447,1,1V14z"
-						/>
-				</g>
-				</svg> to add a new person.
+		<?php foreach ($users as $key => $value): ?>
+			<?php Engine::render('user', array("value"=>$value, "key"=>$key)); ?>
+		<?php endforeach ?>
+		<div class="hint"><span>Press </span>
+			<div class="img">
+				<svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+					 viewBox="0 0 405 408" style="enable-background:new 0 0 405 408;" xml:space="preserve">
+					<style type="text/css">
+						.st0{fill:none;stroke:#40E0D0;stroke-width:30;stroke-miterlimit:10;}
+					</style>
+					<image style="display:none;overflow:visible;" width="50" height="50" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAABjklEQVRoQ+2Z4VHDMAyFXzdghI7Q
+					DaCTQDdgE0aATaATUDZgBJgA7nENp9PFaSrZsS4n/2wjV5/es+y4G6xkbFbCgQSJpmQqkoo0qkBL
+					a91WzvkDwFdpzpYgP5VB9gDeEsRR1VUqYrWwLEYIayWIsHYq4ljnf6HS32mtWtbaAXgySnMn4ror
+					wmRejSAyLEFqWUsrwhZoGfqMJC3Lw+BjYdJqG6IGsVpE5ynnPQKQ60k+myBTleN3cxR5AMB3kcOE
+					B8MrQojnM8DLBExoEAlBlm8AXNSfI8qEBRmDYLKngr1CglwLQbZwIBqCSdJKY3aS4tycbcfPurff
+					LYB3AEzKM7qDMHl2pnsPRQRFhvw1DO+euIcU76AUOJ8rNYXFd3YNw8R4LpsLUxJ1cZAxm9WA6QKi
+					YXiqZZv1qNINZIDhju6F4FxdQZgAW7JHiWHNdAdxduT/8AS59D5Sq9KWeVw3jbVuUSyJ65gEGboP
+					W2mEwQ22y19vi8LPuUxYNCHrjyWItXKt4lKRVpW1zrsaRX4BGZ+aM8/0spwAAAAASUVORK5CYII=" transform="matrix(12.18 0 0 12.18 -102 -100)">
+					</image>
+					<polygon class="st0" points="390,15 390,393 15,393 15,131 186,131 185,15 "/>
+					<polyline class="st0" points="281,162 281,260 113.2,260 "/>
+					<polyline class="st0" points="161.8,320.2 113.2,260 158.5,209.2 "/>
+				</svg>
 			</div>
-		<?php endif ?>
+			<span> to add a new person.</span>
+		</div>
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script src="./js/script.js"></script>
